bf59b560a3f6fc24496c286f0fc8328d23ccc1b4
Joy <joysu2008@gmail.com>
2025-12-08 18:00:55 -0500

    implement projects page logic (filtering buttons)

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,4 @@
 const ProjectCard = ({ project }) => {
-  // const getCategoryColor = (category) => {
-  //   if (category === 'coding') return 'red';
-  //   else if (category === 'teaching') return 'yellow';
-  //   else if (category === 'events') return 'green';
-  //   else if (category === 'research') return 'blue';
-  //   else return 'dark';
-  // }
 
   return (
     <div className="project-card">
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,22 @@
+import React, { useState } from 'react';
+import ProjectCard from "../components/ProjectCard.jsx";
+import { ProjectData } from "../data/ProjectData.jsx";
+
 const Projects = () => {
+  const categories = ['all', 'coding', 'teaching', 'events', 'research']
+	const [activeCategory, setActiveCategory] = useState('all');
 	return (
-		<h1>this is the projects page</h1>
+		<>
+			<h1>this is the projects page</h1>
+			{categories.map((category) => 
+				<button className={`category-select ${category}`} onClick={() => setActiveCategory(category)}>{category}</button>
+			)}
+			{activeCategory === 'all' ? Object.values(ProjectData).flat().map((object) => 
+					<ProjectCard project={object}/>
+				) : ProjectData[activeCategory].map((object) => 
+					<ProjectCard project={object}/>
+			)}
+		</>
 	);
 };
 
