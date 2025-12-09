a9593abaf50d61e152e1a587ba1d67c819feeaa6
Joy <joysu2008@gmail.com>
2025-12-09 14:27:29 -0500

    fisher-yates shuffleArray function for all category

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 import ProjectCard from "../components/ProjectCard.jsx";
 import { ProjectData } from "../data/ProjectData.jsx";
 
+const shuffleArray = (array) => { // this is just the fisher-yates shuffle, idrk how it works but it does!
+	const newArray = [...array];
+	for (let i = newArray.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i+1));
+		[newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+	}
+	return newArray;
+}
+
 const Projects = () => {
   const categories = ['all', 'coding', 'teaching', 'events', 'research']
 	const [activeCategory, setActiveCategory] = useState('all');
@@ -14,7 +23,7 @@ const Projects = () => {
 				)}
 			</div>
 			<div className="projects-container">
-				{activeCategory === 'all' ? Object.values(ProjectData).flat().map((object) => 
+				{activeCategory === 'all' ? shuffleArray(Object.values(ProjectData).flat()).map((object) => 
 						<ProjectCard project={object}/>
 					) : ProjectData[activeCategory].map((object) => 
 						<ProjectCard project={object}/>
