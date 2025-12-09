8b8de9a25f011b53d1e0e3a0d5859aa534c95825
Joy <joysu2008@gmail.com>
2025-12-08 22:24:28 -0500

    implement modal

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,16 +1,35 @@
-const ProjectCard = ({ project }) => {
+import { useState } from 'react';
 
+const ProjectCard = ({ project }) => {
+  const [modal, setModal] = useState(false);
   return (
-    <div className="project-card">
-      <div className={`project-card-header ${project.category}`}>
-        <h1>{project.title}</h1>
+    <>
+      <div className="project-card" onClick={() => setModal(true)}>
+        <div className={`project-card-header ${project.category}`}>
+          <h1>{project.title}</h1>
+        </div>
+        <div className="project-card-content">
+          <p>{project.shortDescription}</p>
+          <img src={project.coverImage}/>
+          <p className="project-card-date">{project.startDate.toLocaleDateString('en-US', {month: 'long', year: 'numeric'}) + " - " + project.endDate.toLocaleDateString('en-US', {month: 'long', year: 'numeric'})}</p>
+        </div>
       </div>
-      <div className="project-card-content">
-        <p>{project.shortDescription}</p>
-        <img src={project.coverImage}/>
-        <p className="project-card-date">{project.startDate.toLocaleDateString('en-US', {month: 'long', year: 'numeric'}) + " - " + project.endDate.toLocaleDateString('en-US', {month: 'long', year: 'numeric'})}</p>
-      </div>
-    </div>
+      {modal && 
+        <div className="project-card-overlay">
+          <div className="project-card">
+            <div className={`project-card-header ${project.category}`}>
+              <h1>{project.title}</h1>
+              <button onClick={() => setModal(false)}>X</button>
+            </div>
+            <div className="project-card-content" style={{overflowY: 'auto'}}>
+              <p>{project.fullDescription}</p>
+              <img src={project.coverImage}/>
+              <p className="project-card-date">{project.startDate.toLocaleDateString('en-US', {month: 'long', year: 'numeric'}) + " - " + project.endDate.toLocaleDateString('en-US', {month: 'long', year: 'numeric'})}</p>
+            </div>
+          </div>
+        </div>
+      }
+    </>
   );
 };
 
