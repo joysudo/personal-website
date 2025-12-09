fbb8ea80152c9e335fa1290692ff1c4664a0252a
Joy <joysu2008@gmail.com>
2025-12-09 14:18:38 -0500

    add full project descriptions

--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -1,68 +1,145 @@
 export const ProjectData = {
     coding: [
         {
-            id: 1,
             title: "Joy's Personal Website",
             category: "coding",
             shortDescription: "this is the short description here. I'm just adding extra text so I can see what it looks like when it's longer.",
             fullDescription: [
-                "I initially made this website in order to get free fudge. But it's also something I've been wanting to do for a while. (If you search 'resume' in my files, you'll find 30 copies of a CV.) Just for my own reference, I thought it would be fun to have a database of most of the projects I've done.", 
-                "This thing is built in React--really a pretty straightforward process! It took me around eight hours of coding, according to Hackatime, and I'd estimate one hour on Figma. Would recommend for any other students during a rainy day off school."
+                "I initially made this website in order to get free fudge. But it's also something I've been wanting to do for a while (if you search 'resume' in my files, you'll find 30 copies of the same CV). Just for my own reference, I thought it would be fun to have a database of most of the projects I've done.", 
+                "This thing is built in React—really a pretty straightforward process! It took me around eight hours of coding, according to Hackatime, and I'd estimate one hour on Figma. Would recommend for any other students during a rainy day off school."
             ],
             startDate: new Date(2024, 0, 15),
             endDate: new Date(2025, 0, 15),
             coverImage: "/placeholder.jpg"
         },
         {
-            id: 2,
-            title: "coding project title 2",
+            title: "Cattatime",
             category: "coding",
             shortDescription: "this is the short description here",
-            fullDescription: ["this is the loooooooooong description"],
+            fullDescription: [
+                "I helped make Cattatime at a <b>Parthenon</b>: the largest all-girls high school hackathon in the world . I met my teammates (Kat and Kaylee!) after a chaotic few hours of navigating transit with a carry-on.",
+                "The app itself is a virtual pet. You can get coins to buy your cat accessories by tracking coding hours in <b>Hackatime</b>.",
+                "I built Cattatime's entire frontend (also handled the backend operation of getting data to save to a local JSON). Kat navigated the backend and Slack integration, while Kaylee drew our visuals. Because we used Electron to turn React into a desktop application, I think a web port would be pretty simple. That's my idea for a next step forward, although we've got plenty of hats to add and more virtual inflation to fix 🫠",
+                "It was a very sleep-deprived 36 hours, but I had tons of fun. Lots of love to my Parthy friends! And I think our eye bags paid off, with Cattatime earning <b>first place</b> ʕ·͡ᴥ·ʔ"
+            ],
             startDate: new Date(2024, 0, 15),
             endDate: new Date(2025, 0, 15),
             coverImage: "/placeholder.jpg"
         },
+        {
+            title: "Logic Lab",
+            category: "coding",
+            shortDescription: "this is the short description here",
+            fullDescription: [
+                "Logic Lab is an e-learning platform that teaches users to <b>identify logical fallacies</b>. Think of it like Duolingo for media literacy.", 
+                "Logic Lab was initially an entry for the Congressional App Challenge. I made a pact with my friend, Tanisha Sajjan, over the summer. She would develop a Chrome extension, using AI to highlight fallacies as users browsed. I would build the site.",
+                "In the end, I spent <b>50-60 hours</b> coding the site. What probably contributed to that lengthy coding time was the fact it was my first time ever using React. I taught myself it while refreshing myself on Javascript, which I had only ever used to make buttons on static websites as a kid."
+            ],
+            startDate: new Date(2024, 0, 15),
+            endDate: new Date(2025, 0, 15),
+            coverImage: "/placeholder.jpg"
+        },
+        {
+            title: "Unburnt Books",
+            category: "coding",
+            shortDescription: "this is the short description here",
+            fullDescription: [
+                "I joined <b>Kode With Klossy</b>'s web development program because my little sister told me she required me for moral support. This was really not a course made for me, as someone already familiar with <b>HTML</b>, <b>CSS</b>, and <b>JS</b>.",
+                "But I loved getting to meet the people in my group, and it was nice having dedicated time to work on a project every day. As a final project, I helped make <b>Unburnt Books</b>. While the UI (sans navigation bar) was mostly designed by my teammates, I did handle the <b>recommender</b>, which fetches data from a collection of banned books I scraped the internet for. And my sister made a website she was proud of—which is all I can really ask for.",
+                "My biggest insights, ironically, mostly surrounded curriculum development. When we design video games, we talk about the 'compulsion loop': <i>anticipation</i> (e.g. buying gear to fight the boss), <i>challenge</i> (e.g. fighting the boss), and <i>reward</i> (e.g. slaying the boss itself). In the beginning stages of learning, I think the goal is quantity of projects over quality. And that's because we're looking for learners to experience the full cycle of the compulsion loop as many times as possible.",
+                "When I'm teaching kids, I'm hoping for this same anticipation while receiving instruction, challenge while applying instruction, and reward from mastery of new skill. Luckily, I think skills like coding really lend themselves to this cycle."
+            ],
+            startDate: new Date(2024, 0, 15),
+            endDate: new Date(2025, 0, 15),
+            coverImage: "/placeholder.jpg"
+        },
+        {
+            title: "John Deere Tech Mentorship",
+            category: "coding",
+            shortDescription: "this is the short description here",
+            fullDescription: [
+                "In a very Iowa fashion, one of the first formal computer science programs I was ever accepted into was a six-month mentorship program at John Deere.",
+                "Tragically, I never got to ride a tractor. But I did get to shadow software engineers, study recursion in Python, and meet some pretty awesome people. I also took the opportunity to build a lot of mini games. The more practice the better, right?",
+                "At the end of our six months, I was invited to give a keynote speech. Miraculously, I have <i>zero</i> pictures of me delivering it. But the twice-weekly sessions opened me up to what actually working in computer science looks like."
+            ],
+            startDate: new Date(2024, 0, 15),
+            endDate: new Date(2025, 0, 15),
+            coverImage: "/placeholder.jpg"
+        }
     ], 
     teaching: [
         {
-            id: 1,
-            title: "teaching project title 1",
+            title: "Pi515: Middle School Programs",
             category: "teaching",
             shortDescription: "this is the short description here",
-            fullDescription: ["this is the loooooooooong description"],
+            fullDescription: [
+                "I worked with Pi515 to create a coding curriculum (that we piloted in three middle schools!). For months, we prepped before entering schools, where I was lucky enough to be able to instruct.",
+                "I miss these kids unbelievably. If you were at a middle school I taught—reach out anytime."
+            ],
             startDate: new Date(2024, 0, 15),
             endDate: new Date(2025, 0, 15),
             coverImage: "/placeholder.jpg"
         },
         {
-            id: 2,
-            title: "teaching project title 2",
+            title: "Sucrates: African American Studies Videos",
             category: "teaching",
             shortDescription: "this is the short description here",
-            fullDescription: ["this is the loooooooooong description"],
+            fullDescription: [
+                "I started making videos for African American Studies for my friends. I didn't expect that I would be asked by other African American Studies students to make more videos for the next three months straight.", 
+                "Now, I'm in the middle of an accidental YouTuber arc. I'm also realizing that no one knows that 'Sucrates' is a pun. You know Socrates? <i>Su</i>-crates? My last name? I'm not just really into crates, I promise.",
+                "<i>You can watch my videos on <a href='https://www.youtube.com/@su.crates'>Youtube</a> or support them on my <a href='https://buymeacoffee.com/sucrates'>Buy Me a Coffee</a> page.</i>"
+            ],
             startDate: new Date(2024, 0, 15),
             endDate: new Date(2025, 0, 15),
             coverImage: "/placeholder.jpg"
         },
+        {
+            title: "Girls Who Code Student Leadership Council",
+            category: "teaching",
+            shortDescription: "this is the short description here",
+            fullDescription: [
+                "I was lucky enough to be selected for the inaugural year of Girls Who Code's Student Leadership Council, serving over 760,000 girls around the world. Together, around ~20 girls from all over worked to design a new mentorship program for women in STEM, eventually presenting our proposal to the GWC programs department.",
+                "The reason this works? There are a few key dropoffs in female stem engagement (one in early education, with big ones also happening in college/early career). The issue is often a confidence gap, not an <i>ability</i> gap."
+            ],
+            startDate: new Date(2024, 0, 15),
+            endDate: new Date(2025, 0, 15),
+            coverImage: "/placeholder.jpg"
+        },
+        {
+            title: "Research Paper Editing Services",
+            category: "teaching",
+            shortDescription: "this is the short description here",
+            fullDescription: [
+                "I've done a lot of weird things for cash—audited programs, pitched a <a href='https://www.businessrecord.com/pi515-celebrates-girls-entrepreneurship-summit-honors-john-pappajohn/'>startup</a>, published <a href='http://alocasia.org/2022/12/21/joy-su-fertilizer/'>writing</a>, and more. As far as side gigs go, then, this one isn't that 'out there.' My newest inpromptu profession has been editing research papers for Chinese PhD students.",
+                "Through a working relationship with a professor at a top Chinese university, I've comprehensively edited ~3-4 research papers thus far. It's a stark shift from my previous experience (editing for a <a href='https://www.polyphonylit.org/EditorBiosV2/su/joy-su'>literary magazine</a> and reviewing college essays), but it's a jump I feel comfortable making. It's also a fun way to keep up with new developments within the scientific sphere."
+            ],
+            startDate: new Date(2024, 0, 15),
+            endDate: new Date(2025, 0, 15),
+            coverImage: "/placeholder.jpg"
+        }
     ], 
     events: [
         {
-            id: 1,
-            title: "events project title 1",
+            title: "Campfire RTP",
             category: "events",
             shortDescription: "this is the short description here",
-            fullDescription: ["this is the loooooooooong description"],
+            fullDescription: [
+                "<i>This project is still ongoing.</i>",
+                "My latest venture into event organizing. <b>Campfire RTP</b> is a game jam for students in the Research Triangle Park.",
+                "This event is part of an initiative organized by <a href='https://hackclub.com/'>Hack Club</a> to host 200 game jams in 200 cities."
+            ],
             startDate: new Date(2024, 0, 15),
             endDate: new Date(2025, 0, 15),
             coverImage: "/placeholder.jpg"
         },
         {
-            id: 2,
-            title: "events project title 2",
+            title: "Unmute!",
             category: "events",
             shortDescription: "this is the short description here",
-            fullDescription: ["this is the loooooooooong description"],
+            fullDescription: [
+                "I organized Unmute in collaboration with <b>Mental Health America</b>. Unmute was a youth performance intended to raise mental health awareness, with 10 performers of various acts.",
+                "We hosted Unmute at the Cabarrus Arts Council in North Carolina. It was a lot of logistical headache—coordinating venue, performers, and funding—but it was incredibly gratifying see through. I led student sessions, designed all promotional material, and conducted outreach for the event."
+            ],
             startDate: new Date(2024, 0, 15),
             endDate: new Date(2025, 0, 15),
             coverImage: "/placeholder.jpg"
@@ -70,25 +147,50 @@ export const ProjectData = {
     ], 
     research: [
         {
-            id: 1,
-            title: "research project title 1",
+            title: "Addressing Vitamin C Deficiency in the Ethiopian Refugee Crisis",
             category: "research",
             shortDescription: "this is the short description here",
-            fullDescription: ["this is the loooooooooong description"],
+            fullDescription: [
+                "Meta-analysis. Awarded Borlaug Scholar and nominated to attend Global Youth Institute in 2023 (total scholars include 151 students from 10 countries).",
+                "Was invited back to return as a Group Facilitator for this event in 2025, where I got to mentor nine brilliant students :)"
+            ],
             startDate: new Date(2024, 0, 15),
             endDate: new Date(2025, 0, 15),
             coverImage: "/placeholder.jpg"
         },
         {
-            id: 2,
-            title: "research project title 2",
+            title: "Alternatively Parameterizing the Bulk Boundary Relationship in Regular Polytopes",
             category: "research",
             shortDescription: "this is the short description here",
-            fullDescription: ["this is the loooooooooong description"],
+            fullDescription: ["Mathematics paper. Placed third place at the North Carolina Student Academy of Science."],
             startDate: new Date(2024, 0, 15),
             endDate: new Date(2025, 0, 15),
             coverImage: "/placeholder.jpg"
         },
+        {
+            title: "Residential Segregation and Climate Risk",
+            category: "research",
+            shortDescription: "this is the short description here",
+            fullDescription: [
+                "Data modeling of relationship between residential segregation and climate risk using Python (Pandas, Scikit-learn, Statsmodels) and Geographic Information System (QGIS).",
+                "Placed 4th in state Technology Student Association conference."
+            ],
+            startDate: new Date(2024, 0, 15),
+            endDate: new Date(2025, 0, 15),
+            coverImage: "/placeholder.jpg"
+        },
+        {
+            title: "Clearing the Air",
+            category: "research",
+            shortDescription: "this is the short description here",
+            fullDescription: [
+                "This was another project I designed for Kode With Klossy. We created an information website that reveals our data analysis on the relationship between particulate matter and COVID-19. This time, though, I was going for a certification in Data Science.",
+                "It's a certification I would recommend for anyone who wants to grasp the basics of analyzing data in Python. I enjoyed most exercises, and it was a fun way to brush up. For <i>Clearing the Air</i>, I also got to brush up on my pixel art skills—one of the first art styles I ever tried.",
+                "I illustrated, analyzed, and wrote <a href='https://joysu20083.wixsite.com/clearingtheair'>Clearing the Air website</a>. Other team members focused on other <a href='https://raissa-nobrega.wixstudio.io/clear-the-air'>informational website pages</a> and research."
+            ],
+            startDate: new Date(2024, 0, 15),
+            endDate: new Date(2025, 0, 15),
+            coverImage: "/placeholder.jpg"
+        }
     ]
 }
-
